76a090a4e7d5f418bf7b88ac20e2eae6a6bb8ffe
ignaciolunaechechuri <lunaechechri.ignacio@gmail.com>
2015-10-26 19:42:25 -0300

    Commit se agrego la funcionabilidad de la idCard 6

--- src/main/webapp/scripts/app/entities/jobOffer/jobOffer-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/jobOffer/jobOffer-dialog.controller.js
@@ -1,31 +1,42 @@
 'use strict';
 
 angular.module('jobvacancyApp').controller('JobOfferDialogController',
-    ['$scope', '$stateParams', '$modalInstance', 'entity', 'JobOffer', 'User',
-        function($scope, $stateParams, $modalInstance, entity, JobOffer, User) {
+	['$scope', '$stateParams', '$modalInstance', 'entity', 'JobOffer', 'User', 'republish',
+		function ($scope, $stateParams, $modalInstance, entity, JobOffer, User, republish) {
 
-        $scope.jobOffer = entity;
-        $scope.users = User.query();
-        $scope.load = function(id) {
-            JobOffer.get({id : id}, function(result) {
-                $scope.jobOffer = result;
-            });
-        };
+			$scope.jobOffer = entity;
+			$scope.users = User.query();
+			$scope.load = function (id) {
+				JobOffer.get({
+					id : id
+				}, function (result) {
+					$scope.jobOffer = result;
+				});
+			};
 
-        var onSaveFinished = function (result) {
-            $scope.$emit('jobvacancyApp:jobOfferUpdate', result);
-            $modalInstance.close(result);
-        };
+			var onSaveFinished = function (result) {
+				$scope.$emit('jobvacancyApp:jobOfferUpdate', result);
+				$modalInstance.close(result);
+			};
 
-        $scope.save = function () {
-            if ($scope.jobOffer.id != null) {
-                JobOffer.update($scope.jobOffer, onSaveFinished);
-            } else {
-                JobOffer.save($scope.jobOffer, onSaveFinished);
-            }
-        };
+			$scope.save = function () {
 
-        $scope.clear = function() {
-            $modalInstance.dismiss('cancel');
-        };
-}]);
+				if ($scope.jobOffer.id != null) {
+					if (republish === false) {
+						JobOffer.save($scope.jobOffer, onSaveFinished);
+					} else {
+						$scope.jobOffer.id = null;
+						JobOffer.update($scope.jobOffer, onSaveFinished);
+					}
+
+				} else {
+					JobOffer.save($scope.jobOffer, onSaveFinished);
+				}
+
+			};
+
+			$scope.clear = function () {
+				$modalInstance.dismiss('cancel');
+			};
+		}
+	]);
